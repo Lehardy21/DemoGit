4eabf11fff5bf6f9a5948312b386c55377906120
Louis <louischatelier21@gmail.com>
2022-07-22 11:52:40 +0200

    init projet

---
+++ src/main/java/model/Personne.java
@@ -0,0 +1,30 @@
+package model;
+
+public class Personne {
+
+	private String nom;
+
+	
+	
+	public Personne(String nom) {
+		super();
+		this.nom = nom;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+		
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Personne [nom=" + nom + "]";
+	} 
+	
+	
+}
---
+++ src/test/java/test/Test.java
@@ -0,0 +1,16 @@
+package test;
+
+import model.Personne;
+
+public class Test {
+
+	public static void main(String [] args) {
+		Personne p = new Personne("jordan");
+		System.out.println(p);
+		
+		
+		
+		
+	}
+	
+}
