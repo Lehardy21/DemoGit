271de6de76f2fd6e861450f36e8e4c06119903a4
Louis <louischatelier21@gmail.com>
2022-07-22 12:30:02 +0200

    ajout prenom personne + del test 2

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -5,7 +5,7 @@ import model.Personne;
 public class Test {
 
 	public static void main(String [] args) {
-		Personne p = new Personne("jordan",29);
+		Personne p = new Personne("jordan",29,"abid");
 		System.out.println(p);
 		
 		
